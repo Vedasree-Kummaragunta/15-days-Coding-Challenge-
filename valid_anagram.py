8cd37a0898d1280cf34dc0c1cb179d939d5d0008
Vedasree-Kummaragunta <147296437+Vedasree-Kummaragunta@users.noreply.github.com>
2025-08-04 21:31:02 +0530

    Add files via upload

---
+++ valid_anagram.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+sol = Solution()
+print(sol.isAnagram("anagram", "nagaram"))  
+print(sol.isAnagram("rat", "car"))          
