e58214259234a7df762e84a650e65ddf0eab4f20
Vedasree-Kummaragunta <147296437+Vedasree-Kummaragunta@users.noreply.github.com>
2025-08-06 18:41:03 +0530

    Add files via upload

---
+++ binearysearch.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
+list=list(map(int,input("enter the values:").split()))
+target=int(input("enter the target value:"))
+sol = Solution()
+print(sol.search(list,target))    
