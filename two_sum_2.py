9ed7e7e65782060beff14e49f469492e88658a5e
Vedasree-Kummaragunta <147296437+Vedasree-Kummaragunta@users.noreply.github.com>
2025-08-07 21:08:09 +0530

    Add files via upload

---
+++ two_sum_2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        left, right = 0, len(numbers) - 1
+
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+
+            if current_sum == target:
+                return [left + 1, right + 1]
+            elif current_sum < target:
+                left += 1
+            else:
+                right -= 1
+sol = Solution()
+print(sol.twoSum([2,7,11,15], 9))   
+print(sol.twoSum([2,3,4], 6))         
+print(sol.twoSum([-1,0], -1))       
