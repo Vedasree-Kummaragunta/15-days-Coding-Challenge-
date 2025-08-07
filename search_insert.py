00f348cb3a52dcc1c802fa818a2e90557b5cb0c9
Vedasree-Kummaragunta <147296437+Vedasree-Kummaragunta@users.noreply.github.com>
2025-08-07 20:58:56 +0530

    Add files via upload

---
+++ search_insert.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
+sol = Solution()
+print(sol.searchInsert([1, 3, 5, 6], 5))  
+print(sol.searchInsert([1, 3, 5, 6], 2))  
+print(sol.searchInsert([1, 3, 5, 6], 7))  
